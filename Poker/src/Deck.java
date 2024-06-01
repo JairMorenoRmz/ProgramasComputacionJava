a6db0bd24d3c21ba42a5947a3c4be951ce95884d
Jair Moreno <jair_moreno90@hotmail.com>
2024-05-31 22:14:48 -0600

    Actividad 3

---
+++ Poker/src/Deck.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class Deck {
+    private List<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+        initializeDeck();
+    }
+
+    private void initializeDeck() {
+        String[] suits = {"Treboles", "Corazones", "Picas", "Diamantes"};
+        String[] colors = {"Negro", "Rojo"};
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "J", "Q", "K"};
+
+        for (String suit : suits) {
+            for (String value : values) {
+                String color = (suit.equals("Treboles") || suit.equals("Picas")) ? colors[0] : colors[1];
+                cards.add(new Card(suit, color, value));
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+        System.out.println("Se mezcló el Deck");
+    }
+
+    public void head() {
+        if (cards.isEmpty()) {
+            System.out.println("No hay cartas en el deck");
+            return;
+        }
+        Card card = cards.remove(0);
+        System.out.println(card);
+        System.out.println("Quedan " + cards.size() + " cartas en el deck");
+    }
+
+    public void pick() {
+        if (cards.isEmpty()) {
+            System.out.println("No hay cartas en el deck");
+            return;
+        }
+        Random rand = new Random();
+        int index = rand.nextInt(cards.size());
+        Card card = cards.remove(index);
+        System.out.println(card);
+        System.out.println("Quedan " + cards.size() + " cartas en el deck");
+    }
+
+    public void hand() {
+        if (cards.size() < 5) {
+            System.out.println("No hay suficientes cartas en el deck");
+            return;
+        }
+        for (int i = 0; i < 5; i++) {
+            Card card = cards.remove(0);
+            System.out.println(card);
+        }
+        System.out.println("Quedan " + cards.size() + " cartas en el deck");
+    }
+
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        deck.shuffle();
+        deck.head();
+        deck.pick();
+        deck.hand();
+    }
+}
